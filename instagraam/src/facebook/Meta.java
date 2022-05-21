31e45ac0a32e3b54a90c3d4138ee7f5b82ac2ce6
Chaman <radhebsr@gmail.com>
2022-05-21 11:06:58 +0530

    update

--- instagraam/src/facebook/Meta.java
+++ instagraam/src/facebook/Meta.java
@@ -3,7 +3,7 @@ package facebook;
 public class Meta {
 
         public static void main (String[] args){
-
+            // just check
             // how to print in console
             System.out.println(" This is my first git");
             System.out.println("I am learing to clone coading in intellj ");
